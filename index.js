199c5e5a89b66ba30a01aa38d0685e530a7a42d4
Tiago Silva <tiagosilva.dev2020@gmail.com>
2021-06-18 19:20:50 -0300

    first

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const puppeteer = require('puppeteer');
+
+app.use(cors());
+app.get('/freelas', (req, res) => {
+  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.br.freelancer.com/jobs/javascript_react-js_angular-js_vue-js_php_java/?languages=pt');
+
+
+    const vagaList = await page.evaluate(() => {
+      const nodeList = document.querySelectorAll('.JobSearchCard-item ')
+      const vagaArray = [...nodeList]
+      const it = [];
+      const vagaList = vagaArray.map((item) => {
+        it.push({
+          titulo: item.querySelectorAll('.JobSearchCard-primary-heading-link')[0].outerText,
+          link: item.querySelectorAll('.JobSearchCard-primary-heading-link')[0].href,
+          dias: item.querySelectorAll('.JobSearchCard-primary-heading-days')[0].outerText,
+          descricao: item.querySelectorAll('.JobSearchCard-primary-description')[0].outerText,
+          oferta: item.querySelectorAll('.JobSearchCard-primary-price')[0].outerText
+        });
+
+      })
+      return it
+    })
+       res.json(vagaList);
+    await browser.close();
+  })();
+});
+
+app.listen(3002, () => {
+  console.log('app rodando');
+})
