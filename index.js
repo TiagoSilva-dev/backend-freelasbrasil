8fc6a68c6098e34b5b2780357f07ee90e54e4df3
Tiago Silva <tiagosilva.dev2020@gmail.com>
2021-06-18 23:25:52 -0300

    finalizado

--- index.js
+++ index.js
@@ -1,42 +1,31 @@
+const request = require('request');
+const cheerio = require('cheerio');
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const puppeteer = require('puppeteer');
-
 app.use(cors());
+
+
+let freelas = [];
+let site = 'https://www.br.freelancer.com';
+request('https://www.br.freelancer.com/jobs/javascript_react-js_angular-js_vue-js_php_java/?languages=pt', function (err, res, body) {
+  var $ = cheerio.load(body);
+
+  $('.JobSearchCard-item').each(function () {
+    freelas.push({
+      titulo: $(this).find('.JobSearchCard-primary-heading-link').text().trim(),
+      link:  site + $(this).find('.JobSearchCard-primary-heading-link').attr('href'),
+      dias: $(this).find('.JobSearchCard-primary-heading-days').text().trim(),
+      descricao: $(this).find('.JobSearchCard-primary-description').text().trim(),
+      oferta: $(this).find('.JobSearchCard-primary-price').text().trim(),
+    });
+  })
+})
+
 app.get('/freelas', (req, res) => {
-  let teste = {
-    nome: 'tiago'
-  }
-  /*
-  (async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://www.br.freelancer.com/jobs/javascript_react-js_angular-js_vue-js_php_java/?languages=pt');
-
-
-    const vagaList = await page.evaluate(() => {
-      const nodeList = document.querySelectorAll('.JobSearchCard-item ')
-      const vagaArray = [...nodeList]
-      const it = [];
-      const vagaList = vagaArray.map((item) => {
-        it.push({
-          titulo: item.querySelectorAll('.JobSearchCard-primary-heading-link')[0].outerText,
-          link: item.querySelectorAll('.JobSearchCard-primary-heading-link')[0].href,
-          dias: item.querySelectorAll('.JobSearchCard-primary-heading-days')[0].outerText,
-          descricao: item.querySelectorAll('.JobSearchCard-primary-description')[0].outerText,
-          oferta: item.querySelectorAll('.JobSearchCard-primary-price')[0].outerText
-        });
-
-      })
-      return it
-    })
-       res.json(vagaList);
-    await browser.close();
-  })();*/
-  res.json(teste);
+  res.json(freelas);
 });
 
-app.listen(process.env.PORT || 3002, () => {
-  console.log('app rodando');
+app.listen(3002, () => {
+  console.log('app rodando na porta ');
 })
